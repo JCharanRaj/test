04718e71db0f57a6a8759ec72e10710894d3742e
charanrajj <charanrajj@wavelabs.ai>
2020-03-21 18:42:22 +0530

    Integrated sms service

--- src/main/java/com/school/canvasing/adapter/MemberAdapter.java
+++ src/main/java/com/school/canvasing/adapter/MemberAdapter.java
@@ -6,39 +6,41 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.school.canvasing.view.ViewResponse;
+import com.school.canvasing.common.Constants;
+import com.school.canvasing.exception.MemberOtpException;
 
-
+@Service
 public class MemberAdapter {
-	
-	@Value("${sms.otp.service}")
-	private String sms_service_url;
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(MemberAdapter.class);
 
-	@Value("${customer.api.service}")
-	private String customerApiUrl;
+	@Value("${sms.key}")
+	private String smsKey;
+
+	@Value("${sms.service}")
+	private String smsServiceUrl;
+	@Value("${sender.id}")
+	private String senderId;
 
 	private RestTemplate restTemplate = new RestTemplate();
 
-	private String getApiUrl(String apiUrl, long id) {
-		StringBuffer buffer = new StringBuffer(apiUrl);
-		buffer.append(id);
-		return apiUrl;
-	}
-	
-		public void getCustomerDetails(String mobileNumber) {
-		//logger.info("getCustomerDetails method started");
-		String apiUrl = sms_service_url+"/"+mobileNumber;
-		try {
-			ResponseEntity<String> result = restTemplate.exchange(apiUrl, HttpMethod.GET, null, String.class);
 
-		} catch (Exception e) {
-			//throw new CustomerNotFoundException(Constants.CUSTOMER_ID_NOT_FOUND + customerId);
+	public String sendOtp(String apiUrl,String mobileNumber) {
+		ResponseEntity<String> response = null;
+		try {
+			response = restTemplate.exchange(apiUrl, HttpMethod.GET, null, String.class);
+
+		} catch (Exception exception) {
+			LOGGER.info("Exception occured while sending otp, Cause:: "+exception.getMessage());
+			throw new MemberOtpException(Constants.OTP_EXCEPTION_MESSAGE+mobileNumber);
 		}
-			//return customerResponse.getBody();
+		if (response.getStatusCode() != HttpStatus.OK) {
+			throw new MemberOtpException(Constants.OTP_EXCEPTION_MESSAGE+mobileNumber);
+		}
+		return response.getBody();		
 	}
 
 }
